7506ac550e0be3cc3a902427c59b69dfc17c3a70
OfficialChamika <chamikarathnayake890@gmail.com>
2021-02-12 17:13:12 +0530

    Initial commit

---
+++ src/main/java/com/chamika/Employee/Manager/Angular/and/Spring/Boot/EmployeeManagerAngularAndSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.chamika.Employee.Manager.Angular.and.Spring.Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagerAngularAndSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagerAngularAndSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chamika/Employee/Manager/Angular/and/Spring/Boot/EmployeeManagerAngularAndSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamika.Employee.Manager.Angular.and.Spring.Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagerAngularAndSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
